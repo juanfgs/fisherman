29481a19152ddd9b47a26cc3096362fb03b6f889
Juan Francisco Giménez Silva <juanfgs@gmail.com>
2015-10-20 23:24:36 -0300

    updated spec file for form helper

--- spec/helpers/form_spec.rb
+++ spec/helpers/form_spec.rb
@@ -5,17 +5,13 @@ require 'nokogiri'
 
 describe Helpers::Form do
   before do
-    @form = Helpers::Form.new(false)
+    @form = Helpers::Form.new()
   end
 
   it "produces a valid text element from a hash describing all it's properties" do
-    @form.add_field "text", "foobar", { :id => 'firstfield', :class => 'klass', :value => 'asdasd', :placeholder => 'bubu'}
-    
+    @form.add_field :text, "foobar", { :id => 'firstfield', :class => 'klass', :value => 'asdasd', :placeholder => 'bubu'}
     doc = Nokogiri::HTML( @form.render )
-
     doc.errors.must_be_empty
-
-
     end
     
   it "produces a valid text element from a hash describing some properties" do
