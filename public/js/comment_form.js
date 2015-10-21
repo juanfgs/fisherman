cb5ef291ebde836d63031925a3b0c01ea0d950d5
Juan Francisco Giménez Silva <juanfgs@gmail.com>
2015-10-20 23:23:29 -0300

    updated js and style

--- public/js/comment_form.js
+++ public/js/comment_form.js
@@ -4,18 +4,18 @@ $(document).ready(function(){
 	$.ajax({
 	    url: '/contacts/' + $(this).attr('data-id'),
 	    success:function(res){
-		
 		var res = JSON.parse(res);
-		console.log(res);
-		$('#currentContact [data-contact=name]').text(res.contact.name);
-		$('#currentContact [data-contact=description]').text(res.contact.description);
-		$('#currentContact [data-contact=status]').text(res.contact.status);
-		$('#currentContact [data-contact=url]').text(res.contact.url);
-		$('#currentContact [data-contact=created_at]').text(res.contact.created_at);
-		$('#commentForm [name=contact_id]').val(res.contact.id);
-		$("#commentList .panel-body").empty();
+
+		$('#currentContact [data-contact=name]').text(res.name);
+		$('#currentContact [data-contact=description]').text(res.description);
+		$('#currentContact [data-contact=status]').text(res.status);
+		$('#currentContact [data-contact=url]').text(res.url);
+		$('#currentContact [data-contact=created_by]').text(res.user.name);		
+		$('#currentContact [data-contact=created_at]').text(res.created_at);
+		$('#commentForm [name=contact_id]').val(res.id);
+		$("#commentList .panel-body .list").empty();
 		res.comments.forEach(function(value,idx){
-		    $("#commentList .panel-body").append('<div class="media"><div class="media-left"></div><div class="media-body">' +value.comment +'</div></div><hr/>');
+		    $("#commentList .panel-body .list").append('<div class="media"><div class="media-left">'+value.user.name+'</div><div class="media-body">' +value.comment +'</div></div><hr/>');
 		});
 	    }
 	});
