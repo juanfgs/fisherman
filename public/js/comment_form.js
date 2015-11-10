ec214108cde1df07961e96660872052a7f6554e4
Juan Francisco Giménez Silva <juanfgs@gmail.com>
2015-11-09 23:13:26 -0300

    updated javascript

--- public/js/comment_form.js
+++ public/js/comment_form.js
@@ -1,36 +1,7 @@
 $(document).ready(function(){
-    
-
 
     // open the contact box and comment box
-    $('.contact').click(function(){
-	$.ajax({
-	    url: '/contacts/' + $(this).attr('data-id'),
-	    success:function(res){
-		var res = JSON.parse(res);
-		window.location.hash = 'contact-' + res.id;
-		$('#currentContact [data-contact=name]').text(res.name);
-		$('#currentContact [data-contact=description]').text(res.description);
-		$('#currentContact [data-contact=status] select').val(res.status);
-		$('#currentContact [data-contact=url]').text(res.url);
-		$('#currentContact [data-contact=id]').val(res.id);		
-		$('#currentContact [data-contact=created_by]').text(res.user.name);		
-		$('#currentContact [data-contact=created_at]').text(moment(res.created_at).format('MMMM Do YYYY, hh:mm:ss'));
-		$('#commentForm [name=contact_id]').val(res.id);
-		$("#commentList .panel-body .list").empty();
-		res.comments.forEach(function(value,idx){
-		    $("#commentList .panel-body .list").append('<div class="media"><div class="media-left">'+value.user.name+'</div><div class="media-body">' +value.comment +'</div></div><hr/>');
-		});
-	    }
-	});
-	
-	if( !$('#cBox').is(":visible") ){
-	    $('#contacts .col-md-12').addClass('col-md-8').removeClass('col-md-12');;
-	    $('#cBox').fadeIn();
-	}
-
-    });
-
+    $('.contact').click(commentBox);
 
     // make contact description and comment box full width
     $(document).on('click', '#toggle-full-width', function(){
@@ -49,7 +20,6 @@ $(document).ready(function(){
 	}
     });
 
-    
     // Change status of contact
     $("[data-contact=status]").change(function(){
 	$.ajax({
@@ -62,10 +32,51 @@ $(document).ready(function(){
 		window.location.reload();
 	
     });
+    
+    // Ask for confirmation when deleting something
+    $(".delete").click(function(ev, options){
 
+	ev.preventDefault();
+	var el = $(this).parent().parent().find("td:first");
+	var confirmation = confirm("Are you sure you want to delete \""+$(el).text()+"\"?")
+	if(confirmation){
+	    window.location.href = $(this).attr('href');
+	}
+	return false;		    
+
+    });
+    
     // trigger click when a contact is referenced by hash
     var hash = window.location.hash;
     $(hash).trigger("click");
 
 });
 
+function commentBox(){
+	$.ajax({
+	    url: '/contacts/' + $(this).attr('data-id'),
+	    success:function(res){
+		var res = JSON.parse(res);
+		window.location.hash = 'contact-' + res.id;
+		$('#currentContact [data-contact=name]').text(res.name);
+		$('#currentContact [data-contact=description]').text(res.description);
+		$('#currentContact [data-contact=status] select').val(res.status);
+		$('#currentContact [data-contact=url]').text(res.url);
+		$('#currentContact [data-contact=id]').val(res.id);		
+		$('#currentContact [data-contact=created_by]').text(res.user.name);		
+		$('#currentContact [data-contact=created_at]').text(moment(res.created_at).format('MMMM Do YYYY, hh:mm:ss'));
+		$('#commentForm [name=contact_id]').val(res.id);
+		$("#commentList .panel-body .list").empty();
+		res.comments.forEach(function(value,idx){
+		    $("#commentList .panel-body .list").append('<div class="media"><div class="media-left">'+value.user.name+'</div><div class="media-body">' +value.comment +'</div></div><hr/>');
+		});
+	    }
+	
+	});
+    if( !$('#cBox').is(":visible") ){
+	$('#contacts .col-md-12').addClass('col-md-8').removeClass('col-md-12');;
+	$('#cBox').fadeIn();
+    }
+
+
+}
---
+++ public/js/search.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+    $("#searchBox").keypress(function(){
+
+	if($(this).val().length >= 3){
+	    $.ajax({
+		url: '/contacts/search/' + $(this).val(),
+		success: function(res){
+		    res = JSON.parse(res);
+		    $("#cList .table tbody").empty();
+		    if(Array.isArray(res)){
+			res.forEach(function(val,idx,el){
+			    var line = " \
+<tr data-id=\""+ val.id +"\"> \
+<td>"+ val.name  +"</td> \
+<td><a href=\""+ val.url +"\" target=\"_blank\">"+val.url+"</a></td> \
+<td><span class=\"badge\">"+ val.status +"</span></td> \
+<td><a href=\"/contacts/"+val.id+"/delete\" class=\"btn-danger delete btn-sm\"><span class=\"glyphicon glyphicon-remove\" id=\"toggle-full-width\"></span> Delete </a></td> \
+</tr> \
+";
+			    var target = $(line).appendTo("#cList .table tbody");
+			});
+		    }
+			$("#cList .table tbody").on("click","tr", commentBox );
+		}
+	
+	    });
+	}
+    });
+});
+
+function commentBox(){
+	$.ajax({
+	    url: '/contacts/' + $(this).attr('data-id'),
+	    success:function(res){
+		var res = JSON.parse(res);
+		window.location.hash = 'contact-' + res.id;
+		$('#currentContact [data-contact=name]').text(res.name);
+		$('#currentContact [data-contact=description]').text(res.description);
+		$('#currentContact [data-contact=status] select').val(res.status);
+		$('#currentContact [data-contact=url]').text(res.url);
+		$('#currentContact [data-contact=id]').val(res.id);		
+		$('#currentContact [data-contact=created_by]').text(res.user.name);		
+		$('#currentContact [data-contact=created_at]').text(moment(res.created_at).format('MMMM Do YYYY, hh:mm:ss'));
+		$('#commentForm [name=contact_id]').val(res.id);
+		$("#commentList .panel-body .list").empty();
+		res.comments.forEach(function(value,idx){
+		    $("#commentList .panel-body .list").append('<div class="media"><div class="media-left">'+value.user.name+'</div><div class="media-body">' +value.comment +'</div></div><hr/>');
+		});
+	    }
+	
+	});
+    if( !$('#cBox').is(":visible") ){
+	$('#contacts .col-md-12').addClass('col-md-8').removeClass('col-md-12');;
+	$('#cBox').fadeIn();
+    }
+
+
+}
